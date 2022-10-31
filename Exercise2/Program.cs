fe398ed0b99d7934b70bdb52d67a6d7b539fb523
Anisa Fauziah <anisa.fauziah.ft21@mail.umy.ac.id>
2022-10-31 16:49:46 +0700

    Add class program

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -1,31 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercise2
+{
+    class Program
     {
-        class Program
-        {
+        //array of integers to hold values
+        private int[] anisa = new int[61];
+        private int cmp_count = 0; // number of comparasion
+        private int mov_count = 0; // number of data movements
+
+        // Number of elements in array
+        private int n;
 
 
-        //Array to be searched
-        int[] anisa = new int[61];
-        //Number of elements in the array
-        int n;
-        //Get the number of elements to store in the array
-        int i;
-        public void input()
-        {
-            while (true)
-            {
-                Console.Write("Enter the number of the elements in the array: ");
-                string s = Console.ReadLine();
-                n = Int32.Parse(s);
-                if ((n > 0) && (n <= 61))
-                    break;
-                else
-                    Console.WriteLine("|rArray should have minimum 1 and maximum 61 elements.\n");
-            }
-        }
+    }
+
